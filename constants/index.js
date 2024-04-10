1f4d046901e29f05780b755e7d0ca50c58f821f3
kindast <kindast@proton.me>
2024-04-10 23:50:04 +0300

    https

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,6 @@ import icons from "./icons";
 import images from "./images";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 //const api = "http://10.0.2.2:5254";
-const api = "http://booknet.site";
+const api = "https://booknet.site";
 
 export { images, icons, COLORS, FONT, SIZES, SHADOWS, api };
